6a0e1cb99df97f8469d2da7288a8ef6a7b288355
Art3mka <87860402+Art3mka@users.noreply.github.com>
2023-04-26 17:31:00 +0300

    Updates

---
+++ precache-manifest.1ca100a0141835766758850cc783cb2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff9ec113d96904282cd9",
+    "url": "/mining_express-front/static/css/main.3826c147.chunk.css"
+  },
+  {
+    "revision": "ff9ec113d96904282cd9",
+    "url": "/mining_express-front/static/js/main.ff9ec113.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/mining_express-front/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/mining_express-front/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "c8722c8bff81207565fe",
+    "url": "/mining_express-front/static/js/runtime~main.c8722c8b.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/mining_express-front/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "6c9a2b712b02e2fdfc4338b73446ab70",
+    "url": "/mining_express-front/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "art3mka.github.io/mining_express-front/precache-manifest.e88c4c0526e2bbfe8837bb9e20269372.js"
+  "/mining_express-front/precache-manifest.1ca100a0141835766758850cc783cb2c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("art3mka.github.io/mining_express-front/index.html", {
+workbox.routing.registerNavigationRoute("/mining_express-front/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
